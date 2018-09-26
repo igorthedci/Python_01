05d1f7d83f6c4072afbbe240008a3515cd1e651f
igorthedci <42576193+igorthedci@users.noreply.github.com>
2018-09-27 01:30:43 +0300

    Update lesson02task04.py
    @elpiankova
    IF operator updated

--- lesson02task04.py
+++ lesson02task04.py
@@ -4,5 +4,10 @@
 # Если треугольник существует, выведите строку YES, иначе выведите строку NO.
 #
 a,b,c = 123,267,214
-valid = "NO" if (a + b <= c) or (b + c <= a) or (c + a <= b) else "YES"
+if (a + b <= c) or (b + c <= a) or (c + a <= b):
+  valid = "NO"
+else
+  valid = "YES"
 print(valid)
+
+# valid = "NO" if (a + b <= c) or (b + c <= a) or (c + a <= b) else "YES"
