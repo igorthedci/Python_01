4f84ca9c152cdb06f9efee1622208c67ada49bd7
igorthedci <foumart@ukr.net>
2018-10-21 00:33:50 +0300

    class work 06

--- cw06_04_requests.py
+++ cw06_04_requests.py
@@ -20,3 +20,15 @@ headers = {'User-Agent': 'cw06_04'}
 r = requests.get(url, headers=headers, params=dict_params)
 print(r.text)
 print(r.url)
+#
+r_dict = r.json()
+print(type(r_dict))
+print(r_dict['headers']['Host'])
+print(r.url)
+#
+url = 'http://httpbin.org/get'
+dict_params = {"name": "Olena", "q": "Python"}
+headers = {'User-Agent': 'cw06_04'}
+r = requests.post(url, headers=headers, params=dict_params, data={"username": '111', password='***'})
+print(r.status_code)
+#
