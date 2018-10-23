8f3c2cba79d2806a12ced7cfae9cd65ac63e1bc7
igorthedci <foumart@ukr.net>
2018-10-24 00:05:46 +0300

    homework 06-1. def dict_contains

--- hw06-01.py
+++ hw06-01.py
@@ -13,39 +13,55 @@ import requests
 
    Попробуйте воспользоваться http.client вместо requests. Ощутите разницу
 """
-url_books = 'http://pulse-rest-testing.herokuapp.com/books'
-url_roles = 'http://pulse-rest-testing.herokuapp.com/roles'
 
-# ● Создаёт персонажа POST /roles/, вы запоминаете его id.
-hero_01 = {'name': 'Name1', 'type': 'type1', 'level': 1, 'book': 2}
-# r = requests.post(url_roles, data=hero_01)
-# print(r.status_code)
-# r_dict = r.json()
-# print(r_dict)
-# hero_01_id = r.json()['id']
-hero_01_id = 12
-# print(hero_01_id)
-
-# ● Проверяете, что он создался и доступен по ссылке GET /roles/[id]
-hero_test_url = url_roles+'/'+str(hero_01_id)
-# print(hero_test_url)
-hero_test = requests.get(hero_test_url).json()
-marker = True
-for key in hero_01:
-    if key not in hero_test or hero_01[key] != hero_test[key]:
-        marker = False
-        break
-print(marker)
-
-# r2 = requests.delete(url_books+'/'+str(r_dict['id']))
-# print(r2.status_code)
-# print(r2.url)
-# print(r2.json())
-# r2_dict = r2.json()
-# print(r2_dict)
+def dict_contains(a={}, b={}):
+    """
+    the function checks A-dictionary contains B-dictionary
+    :param a:
+    :param b:
+    :return: TRUE if A contains B
+    """
+    result = True
+    for key in b:
+        if key not in a or a[key] != b[key]:
+            result = False
+            break
+    return result
 
 
 if __name__ == '__main__':
-    pass
-#
-#
+
+    url_books = 'http://pulse-rest-testing.herokuapp.com/books'
+    url_roles = 'http://pulse-rest-testing.herokuapp.com/roles'
+
+    # ● Создаёт персонажа POST /roles/, вы запоминаете его id.
+    hero_01 = {'name': 'Name1', 'type': 'type1', 'level': 1, 'book': 2}
+    # r = requests.post(url_roles, data=hero_01)
+    # print(r.status_code)
+    # r_dict = r.json()
+    # print(r_dict)
+    # hero_01_id = r.json()['id']
+    hero_01_id = 12
+    # print(hero_01_id)
+
+    # ● Проверяете, что он создался и доступен по ссылке GET /roles/[id]
+    hero_test_url = url_roles+'/'+str(hero_01_id)
+    hero_test = requests.get(hero_test_url).json()
+    marker = dict_contains(hero_test, hero_01)
+    print('Персонаж', 'создан' if marker else 'отсутствует')
+
+    # ● Проверяете, что он есть в списке пользователей по GET /roles/
+    hero_test_url = url_roles
+    hero_arr = requests.get(hero_test_url).json()
+    marker = False
+    for hero_test in hero_arr:
+        marker = dict_contains(hero_test, hero_01)
+        if marker:
+            break
+    print('Персонаж', 'создан' if marker else 'отсутствует')
+
+    # ● Изменяете этого пользователя методом PUT roles/[id]/
+
+
+    #
+    #
