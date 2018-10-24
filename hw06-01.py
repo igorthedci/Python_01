227cb4e322853647a172b67d9d8fcd67750bfca0
igorthedci <foumart@ukr.net>
2018-10-24 10:20:50 +0300

    hw06-1-8

--- hw06-01.py
+++ hw06-01.py
@@ -11,7 +11,7 @@ import requests
 7● Удаляете этого персонажа методом DELETE roles/[id]
 8● Второй скрипт: тоже самое с книгами
 
-   Попробуйте воспользоваться http.client вместо requests. Ощутите разницу
+9● Попробуйте воспользоваться http.client вместо requests. Ощутите разницу
 """
 
 def dict_contains(a={}, b={}):
@@ -21,9 +21,9 @@ def dict_contains(a={}, b={}):
     :return: TRUE if A-dict contains B-dict
     """
     result = True
-    for key in hero_01:
-        if key not in hero_test or hero_01[key] != hero_test[key]:
-            marker = False
+    for key in b:
+        if key not in a or a[key] != b[key]:
+            result = False
             break
     return result
 
@@ -39,7 +39,7 @@ if __name__ == '__main__':
 #    hero_01_id = 12
 
 # 2● Проверяете, что он создался и доступен по ссылке GET /roles/[id]
-    hero_test_url = url_roles+'/'+str(hero_01_id)
+    hero_test_url = url_roles + '/' + str(hero_01_id)
     hero_test = requests.get(hero_test_url).json()
     marker = dict_contains(hero_test, hero_01)
     print('Персонаж', 'существует' if marker else 'отсутствует.')
@@ -54,13 +54,13 @@ if __name__ == '__main__':
 
 # 4● Изменяете этого персонажа методом PUT roles/[id]/
     hero_01 = {'name': 'Name1new', 'type': 'type1new', 'level': 2, 'book': 2}
-    hero_test_url = url_roles+'/'+str(hero_01_id)
+    hero_test_url = url_roles + '/' + str(hero_01_id)
     hero_test = requests.put(hero_test_url, hero_01).json()
     marker = dict_contains(hero_test, hero_01)
     print('Персонаж', 'обновлен' if marker else 'остался прежним.')
 
 # 5● Проверяете, что он изменился и доступен по ссылке /roles/[id]
-    hero_test_url = url_roles+'/'+str(hero_01_id)
+    hero_test_url = url_roles + '/' + str(hero_01_id)
     hero_test = requests.get(hero_test_url).json()
     marker = dict_contains(hero_test, hero_01)
     print('Изменение', 'правильно.' if marker else 'отсутствует.')
@@ -74,6 +74,49 @@ if __name__ == '__main__':
     print('Изменение', 'правильно.' if marker else 'отсутствует.')
 
 # 7● Удаляете этого персонажа методом DELETE roles/[id]
-    hero_test_url = url_roles+'/'+str(hero_01_id)
+    hero_test_url = url_roles + '/' + str(hero_01_id)
     hero_test = requests.delete(hero_test_url)
     print('Персонаж', hero_01_id, 'удален.' if hero_test.status_code == 204 else 'выжил.')
+
+# 8● Второй скрипт: тоже самое с книгами
+    book_01 = {'title': 'New Book', 'author': 'New Author'}
+    book_01_id = requests.post(url_books, book_01).json()['id']
+#    book_01_id = 820
+    print('Создана книга с номером', book_01_id)
+#
+    book_test_url = url_books + '/' + str(book_01_id)
+    book_test = requests.get(book_test_url).json()
+    marker = dict_contains(book_test, book_01)
+    if not marker:
+        print('Запрос:', book_01)
+        print('Ответ:', book_test)
+    print('Книга', 'существует' if marker else 'отсутствует.')
+#
+    book_list = requests.get(url_books).json()
+    marker = False
+    for book_test in book_list:
+        marker = dict_contains(book_test, book_01)
+        if marker: break
+    print('Книга', 'существует' if marker else 'отсутствует.')
+#
+    book_01 = {'title': 'Next Book', 'author': 'Edition 2'}
+    book_test_url = url_books + '/' + str(book_01_id)
+    book_test = requests.put(book_test_url, book_01).json()
+    marker = dict_contains(book_test, book_01)
+    print('Книга', 'переписана' if marker else 'осталась прежней.')
+#
+    book_test_url = url_books + '/' + str(book_01_id)
+    book_test = requests.get(book_test_url).json()
+    marker = dict_contains(book_test, book_01)
+    print('Изменение', 'правильно.' if marker else 'отсутствует.')
+#
+    book_list = requests.get(url_books).json()
+    marker = False
+    for book_test in book_list:
+        marker = dict_contains(book_test, book_01)
+        if marker: break
+    print('Изменение', 'правильно.' if marker else 'отсутствует.')
+#
+    book_test_url = url_books+'/'+str(book_01_id)
+    book_test = requests.delete(book_test_url)
+    print('Книга', book_01_id, 'удалена.' if book_test.status_code == 204 else 'сохранилась.')
