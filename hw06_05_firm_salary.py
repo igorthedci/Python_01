cc8bbf6c16565cc6f025c03b6b68dd0035de284b
igorthedci <foumart@ukr.net>
2018-10-26 10:32:39 +0300

    hw06_05 processing...

--- hw06_05_firm_salary.py
+++ hw06_05_firm_salary.py
@@ -15,15 +15,20 @@ if __name__ == '__main__':
 #       in_handle = open(in_file, encoding='utf-8')
 #      out_handle = open(out_file, 'w', encoding='utf-8')
 #
+    arr_person = []  # item = { id: Имя_Фамилия, dept: Отдел, salary: Зарплата }
     with open(in_file, encoding='utf-8') as in_handle:
-        with open(out_file, 'wt', encoding='utf-8') as out_handle:
-            aline = in_handle.readline()
-            titles = aline.split(' ')
-            arr_person = []
-            for aline in in_handle:
-                aline = aline.split(' ')
-                arr_person.append( { aline[0]+'_'+aline[1] : (aline[2], aline[3]) } )
-#
+        aline = in_handle.readline()
+        titles = aline.split(' ')
+        key_id = titles[1] + '_' + titles[0]
+        key_dept = titles[2]
+        key_salary = titles[3]
+        for aline in in_handle:
+            aline = aline.split(' ')
+            arr_person.append( { key_id: aline[1]+'_'+aline[0], key_dept: aline[2], key_salary: aline[3] } )
+# 1 Посчитайте сколько отделов на фирме
+    arr_dept = []  # item = { id: counter, title: Отдел }
+    for aperson in arr_person:
+
             arr_dept = []  # item of the array = {title : person_with_max_salary}
             for aperson in arr_person:
 
@@ -31,3 +36,5 @@ if __name__ == '__main__':
                     if
                 else:
                     arr_dept.append((aperson[2],aperson[3]))
+
+        with open(out_file, 'wt', encoding='utf-8') as out_handle:
