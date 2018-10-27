a0a85d83ae9fe0f81eb75b3a1099536f6677b934
igorthedci <foumart@ukr.net>
2018-10-27 16:47:57 +0300

    classwork 07

--- cw07_02.py
+++ cw07_02.py
@@ -1,14 +1,17 @@
 
 import unittest
+from HtmlTestRunner import HTMLTestRunner
 
 
 class OutClass(unittest.TestCase):
 
     def test_01(self):
-        self.assertNotEquals(4, 7)
+        self.assertNotEqual(4, 7)
 
     def test_02(self):
-        self.assertEquals(7, 7)
+        self.assertEqual(7, 7)
 
 
-unittest.main()
+# unittest.main()
+# unittest.main(testRunner=HTMLTestRunner(output=r"./"))
+unittest.main(verbosity=2, testRunner=HTMLTestRunner(output=r"./"))
