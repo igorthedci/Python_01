67844d1ef8f537770c0cc76a7e1f404b897724c1
igorthedci <foumart@ukr.net>
2018-11-20 17:58:25 +0200

    hw08 selenium test

--- hw08_01.py
+++ hw08_01.py
@@ -1,8 +1,8 @@
 import unittest
-from Selenium.webdriver import Firefox
+from selenium.webdriver import Firefox
 
 
-class Wrapper(object):
+class Wrapper(object):  # pattern SINGLETON
     _instance = None
 
     def __new__(cls, *args, **kwargs):
@@ -15,24 +15,59 @@ class Wrapper(object):
         return self.connection
 
 
+class Element(object):
+    def __init__(self, locator):
+        self._locator = locator
+
+
+class TextElement(Element):
+    def __set__(self, obj, val):
+        e = Wrapper().connection.find_element_by_name(self._locator)
+        e.send_keys(val)
+
+    def __get__(self, obj, cls=None):
+        try:
+            e = Wrapper().connection.find_element_by_name(self._locator)
+            return str(e.text)
+        except Exception as err:
+            raise err
+
+
+class LinkElement(Element):
+    def __get__(self, obj, cls=None):
+        try:
+            WebDriverWait(Wrapper().connection, 30).until(lambda driver: driver.find_element_by_xpath(self._locator))
+            e = Wrapper().connection.find_element_by_name(self._locator)
+            return str(e.text)
+        except Exception as err:
+            raise err
+
+
 class GoogleHomePage(object):
+    _url = "http://google.com"
+    locators = {
+        "search": "q",
+        "first_link": "(//a[ancestor::div/@id='ires'])[1]"
+    }
+    search = TextElement(locators["search"])
+    first_link = LinkElement(locators["first_link"])
+
     def open(self):
         Wrapper().connection.get(self._url)
 
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
-        self.driver = Firefox()
-        self.driver.implicity.wait(30)
+        self.driver = Wrapper().remote_webdriver()  # Firefox()
+        self.driver.implicitly_wait(30)
 
     def tearDown(self):
         self.driver.close()
 
 
 class GoogleTesting(BaseTestCase):
-
-    def test_search(self):
-        page = GoogleHomePage()
+    def test_search1(self, page=GoogleHomePage()):
+        # page = GoogleHomePage()
         page.open()
-        page.search = "Automation testing"
+        page.search = "Automated testing"
         assert "Test automation" in page.first_link
